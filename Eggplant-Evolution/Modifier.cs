655fe993fba13c74d560e1f333b3e201b4d18149
Tim Stadler <timstadler905@googlemail.com>
2016-08-04 11:50:30 +0200

    More basic resource implementation

---
+++ Eggplant-Evolution/Modifier.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eggplant_Evolution
+{
+  /// <summary>
+  /// Possible types of modifiers.
+  /// </summary>
+  enum ModifierType
+  {
+    /// <summary>
+    /// A direct "per second" value that will be
+    /// added to the resource.
+    /// </summary>
+    Direct,
+
+    /// <summary>
+    /// A percentual value that will be added
+    /// to the sum of direct modifiers.
+    /// </summary>
+    Percentual
+  }
+
+  class Modifier
+  {
+    #region Properties
+
+    /// <summary>
+    /// A unique identifier of this specific modifier.
+    /// For example a specific building always has the
+    /// identifier "Building X" or so.
+    /// </summary>
+    public string Identifier
+    {
+      get { return _identifier; }
+      private set { _identifier = value; }
+    }
+    private string _identifier;
+
+    /// <summary>
+    /// The value of this modifier.
+    /// </summary>
+    public double Value
+    {
+      get { return _value; }
+      set
+      {
+        if (value == 0)
+          throw new ArgumentException("Value can't be 0");
+        _value = value;
+      }
+    }
+    private double _value;
+
+    /// <summary>
+    /// The type of this modifier.
+    /// </summary>
+    public ModifierType Type
+    {
+      get { return _type; }
+      private set { _type = value; }
+    }
+    private ModifierType _type;
+
+    #endregion Properties
+
+    #region Construction
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="identifier">Source identifier.</param>
+    /// <param name="value">Value.</param>
+    /// <param name="type">Modifier type.</param>
+    public Modifier(string identifier, double value, ModifierType type)
+    {
+      Identifier = identifier;
+      Value = value;
+      Type = type;
+    }
+
+    #endregion Construction
+  }
+}
---
+++ Eggplant-Evolution/Resource.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eggplant_Evolution
+{
+  /// <summary>
+  /// Base class for all resources.
+  /// </summary>
+  class Resource
+  {
+    #region Properties
+
+    /// <summary>
+    /// The name of this resource.
+    /// </summary>
+    public string Name
+    {
+      get { return _name; }
+      private set { _name = value; }
+    }
+    private string _name;
+
+    /// <summary>
+    /// Current amount/value of this resource.
+    /// </summary>
+    public double Value
+    {
+      get { return _value; }
+      set { _value = value; }
+    }
+    private double _value;
+
+    /// <summary>
+    /// The current per second value.
+    /// </summary>
+    public double PerSecond
+    {
+      get { return _perSecond; }
+      private set { _perSecond = value; }
+    }
+    private double _perSecond;
+
+    /// <summary>
+    /// List of currently applied modifiers.
+    /// </summary>
+    public ObservableCollection<Modifier> Modifiers
+    {
+      get { return _modifiers; }
+      private set
+      {
+        if (value == null)
+          throw new ArgumentException("Modifiers can't be null");
+        _modifiers = value;
+      }
+    }
+    private ObservableCollection<Modifier> _modifiers;
+
+    #endregion Properties
+
+    #region Construction
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="name">Name of this resource.</param>
+    public Resource(string name)
+    {
+      Name = name;
+      Modifiers = new ObservableCollection<Modifier>();
+      Modifiers.CollectionChanged += Modifiers_CollectionChanged;
+    }
+
+    /// <summary>
+    /// Updates the <see cref="PerSecond"/> value when a modifier
+    /// gets added or removed from <see cref="Modifiers"/>.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Modifiers_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+      double basePerSecond = Modifiers.Where(i => i.Type == ModifierType.Direct).Sum(i => i.Value);
+      double percentage = 100 + Modifiers.Where(i => i.Type == ModifierType.Percentual).Sum(i => i.Value);
+
+      PerSecond = (percentage * basePerSecond) / 100;
+    }
+
+    #endregion Construction
+  }
+}
